78927a41f4d62cc0c14f2ae32a019e4c9382b99b
fnamlah <faisalmalnamlah@gmail.com>
2023-05-01 18:32:53 +0300

    fourth  commit

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def create_app(db_URI="", test_config=None):
 
     @app.route("/")
     def home():
-        return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))d'
+        return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
     
     @app.route("/login")
     def login():
